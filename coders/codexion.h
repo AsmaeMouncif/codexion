05deb722627e8b67fa6989ed6402a6ee45cfa550
ASMAE MOUNCIF <158608762+AsmaeMouncif@users.noreply.github.com>
2026-05-01 12:37:04 +0000

    v1

--- coders/codexion.h
+++ coders/codexion.h
@@ -1,97 +1,11 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   codexion.h                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/19 15:08:00 by asmounci          #+#    #+#             */
-/*   Updated: 2026/04/26 09:15:36 by codespace        ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef CODEXION_H
 # define CODEXION_H
 
 # include <unistd.h>
 # include <stdlib.h>
 # include <string.h>
-# include <pthread.h>
-# include <sys/time.h>
-# include <stdio.h>
-
-typedef struct s_sim t_sim;
-
-typedef struct s_params {
-    int number_of_coders;
-    int time_to_burnout;
-    int time_to_compile;
-    int time_to_debug;
-    int time_to_refactor;
-    int number_of_compiles_required;
-    int dongle_cooldown;
-    char *scheduler;
-} t_params;
-
-typedef struct s_request
-{
-    int coder_id;
-    long arrival_time;
-    long deadline;
-} t_request;
-
-typedef struct s_dongle {
-    pthread_mutex_t mutex;
-    pthread_cond_t  cond;
-    long last_release_time;
-    int in_use;
-    t_request *heap;
-    int heap_size;
-    int heap_capacity;
-    int initialized;
-} t_dongle;
-
-typedef struct s_coder {
-    int id;
-    int nb_compiles;
-    long last_compile_time;
-    pthread_t thread;
-    t_sim *sim;
-    pthread_mutex_t mutex;
-} t_coder;
-
-typedef struct s_sim {
-    t_params params;
-    t_coder *coders;
-    t_dongle *dongles;
-    int stop;
-    long start_time;
-    pthread_t monitor;
-    pthread_mutex_t log_mutex;
-    int log_mutex_initialized;
-    pthread_mutex_t stop_mutex;
-    int stop_mutex_initialized;
-} t_sim;
 
 int	is_valid_number(char *str);
 int	is_valid_scheduler(char *str);
-int init_sim(t_sim *sim, char **av);
-void cleanup_sim(t_sim *sim);
-long get_time_ms(void);
-void sleep_ms(long ms);
-int sim_should_stop(t_sim *sim);
-void sim_set_stop(t_sim *sim, int value);
-int coder_get_nb_compiles(t_coder *coder);
-void coder_inc_nb_compiles(t_coder *coder);
-long coder_get_last_compile_time(t_coder *coder);
-void coder_set_last_compile_time(t_coder *coder, long time);
-void log_state(t_sim *sim, int id, char *msg);
-void heap_push(t_dongle *dongle, t_request req, char *scheduler);
-void heap_pop(t_dongle *dongle, char *scheduler);
-t_request heap_peek(t_dongle *dongle);
-void dongle_acquire(t_sim *sim, int coder_id, int dongle_id);
-void dongle_release(t_sim *sim, int dongle_id);
-void *coder_routine(void *arg);
-void *monitor_routine(void *arg);
 
 #endif
--- coders/main.c
+++ coders/main.c
@@ -1,83 +1,19 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/19 15:11:13 by asmounci          #+#    #+#             */
-/*   Updated: 2026/04/26 09:16:57 by codespace        ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "codexion.h"
 
-static int  launch_threads(t_sim *sim)
+int	main(int ac, char **av)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (i < sim->params.number_of_coders)
-    {
-        if (pthread_create(&sim->coders[i].thread, NULL,
-                coder_routine, &sim->coders[i]) != 0)
-        {
-            sim_set_stop(sim, 1);
-            while (i < sim->params.number_of_coders)
-                pthread_cond_broadcast(&sim->dongles[i++].cond);
-            return (1);
-        }
-        i++;
-    }
-    if (pthread_create(&sim->monitor, NULL, monitor_routine, sim) != 0)
-    {
-        sim_set_stop(sim, 1);
-        i = 0;
-        while (i < sim->params.number_of_coders)
-            pthread_cond_broadcast(&sim->dongles[i++].cond);
-        return (1);
-    }
-    return (0);
-}
-
-static void join_threads(t_sim *sim)
-{
-    int i;
-
-    pthread_join(sim->monitor, NULL);
-    i = 0;
-    while (i < sim->params.number_of_coders)
-    {
-        pthread_join(sim->coders[i].thread, NULL);
-        i++;
-    }
-}
-
-int main(int ac, char **av)
-{
-    t_sim   sim;
-    int     i;
-
-    if (ac != 9)
-        return (write(2, "Error: invalid number of arguments\n", 35), 1);
-    i = 1;
-    while (i <= 7)
-    {
-        if (!is_valid_number(av[i]))
-            return (write(2, "Error: invalid numeric argument\n", 32), 1);
-        i++;
-    }
-    if (!is_valid_scheduler(av[8]))
-        return (write(2, "Error: invalid scheduler\n", 25), 1);
-    if (init_sim(&sim, av) != 0)
-        return (write(2, "Error: init failed\n", 19), 1);
-    if (launch_threads(&sim) != 0)
-    {
-        join_threads(&sim);
-        cleanup_sim(&sim);
-        return (1);
-    }
-    join_threads(&sim);
-    cleanup_sim(&sim);
-    return (0);
+	if (ac != 9)
+		return (write(2, "Error: invalid number of arguments\n", 35), 1);
+	i = 1;
+	while (i <= 7)
+	{
+		if (!is_valid_number(av[i]))
+			return (write(2, "Error: invalid numeric argument\n", 32), 1);
+		i++;
+	}
+	if (!is_valid_scheduler(av[8]))
+		return (write(2, "Error: invalid scheduler\n", 25), 1);
+	return (0);
 }
