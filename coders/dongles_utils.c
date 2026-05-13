586572c7c69ae327a02b8b4c97fb341ebeebd01b
ASMAE MOUNCIF <158608762+AsmaeMouncif@users.noreply.github.com>
2026-05-13 08:54:19 +0000

    ok

---
+++ coders/dongles_utils.c
@@ -0,0 +1,53 @@
+#include "codexion.h"
+
+long	get_key(t_coder *coder, t_sim *sim)
+{
+	if (sim->params.scheduler == 0)
+		return (get_time_ms());
+	return (coder->last_compile_time + sim->params.time_to_burnout);
+}
+
+int	can_take(t_dongle *d, int coder_id, int cooldown)
+{
+	if (!d->available)
+		return (0);
+	if ((get_time_ms() - d->released_at) < cooldown)
+		return (0);
+	if (heap_top_id(d) != coder_id)
+		return (0);
+	return (1);
+}
+
+void	take_one_dongle(t_coder *coder, t_sim *sim, int idx)
+{
+	t_dongle	*d;
+	t_waiter	w;
+
+	d = &sim->dongles[idx];
+	w.coder_id = coder->id;
+	w.key = get_key(coder, sim);
+	pthread_mutex_lock(&d->mutex);
+	heap_push(d, w);
+	while (!is_stopped(sim)
+		&& !can_take(d, coder->id, sim->params.dongle_cooldown))
+		pthread_cond_wait(&d->cond, &d->mutex);
+	if (is_stopped(sim))
+	{
+		heap_remove(d, coder->id);
+		pthread_mutex_unlock(&d->mutex);
+		return ;
+	}
+	heap_pop(d);
+	d->available = 0;
+	pthread_mutex_unlock(&d->mutex);
+	log_state(sim, coder->id, "has taken a dongle");
+}
+
+void	release_one(t_sim *sim, int idx)
+{
+	pthread_mutex_lock(&sim->dongles[idx].mutex);
+	sim->dongles[idx].available = 1;
+	sim->dongles[idx].released_at = get_time_ms();
+	pthread_cond_broadcast(&sim->dongles[idx].cond);
+	pthread_mutex_unlock(&sim->dongles[idx].mutex);
+}
