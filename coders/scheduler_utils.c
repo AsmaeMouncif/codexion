04c9ab8f83c9029b0f60911e4c1930f99368647c
ASMAE MOUNCIF <158608762+AsmaeMouncif@users.noreply.github.com>
2026-05-14 08:16:10 +0000

    ok

--- coders/scheduler_utils.c
+++ coders/scheduler_utils.c
@@ -16,7 +16,10 @@ void	sift_up(t_dongle *d, int index)
 	while (index > 0)
 	{
 		parent = (index - 1) / 2;
-		if (d->heap[index].key >= d->heap[parent].key)
+		if (d->heap[index].key > d->heap[parent].key)
+			break ;
+		if (d->heap[index].key == d->heap[parent].key
+			&& d->heap[index].coder_id >= d->heap[parent].coder_id)
 			break ;
 		swap_waiters(&d->heap[index], &d->heap[parent]);
 		index = parent;
@@ -34,9 +37,15 @@ void	sift_down(t_dongle *d, int index)
 		left = 2 * index + 1;
 		right = 2 * index + 2;
 		min = index;
-		if (left < d->nb_waiters && d->heap[left].key < d->heap[min].key)
+		if (left < d->nb_waiters
+			&& (d->heap[left].key < d->heap[min].key
+				|| (d->heap[left].key == d->heap[min].key
+					&& d->heap[left].coder_id < d->heap[min].coder_id)))
 			min = left;
-		if (right < d->nb_waiters && d->heap[right].key < d->heap[min].key)
+		if (right < d->nb_waiters
+			&& (d->heap[right].key < d->heap[min].key
+				|| (d->heap[right].key == d->heap[min].key
+					&& d->heap[right].coder_id < d->heap[min].coder_id)))
 			min = right;
 		if (min == index)
 			break ;
